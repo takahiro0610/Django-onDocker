d34d10bede932771ce84bd3b73876369c37990cb
takahiro0610 <ookamikujira@gmail.com>
2023-07-15 01:49:07 +0900

    ほむほむ

--- todolist/models.py
+++ todolist/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+CHOICE = (("danger","high"),("primary","nomal"),("secondary","low"))
 
 class TodoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    priority = models.CharField(
+        max_length = 50,
+        choices = CHOICE,
+        default='low'
+        )
+    duedate = models.DateField(default=datetime.now)
     
     def __str__(self):
         return self.title
